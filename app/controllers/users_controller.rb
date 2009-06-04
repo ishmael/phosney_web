e14c62a9cd4501db7b30ff2eeed8dd4ec69be9b3
Manuel Silva <ishmael@blackbalto.com>
2009-06-04 01:31:59 +0100

    starting to customize the layout
    added the login, last login, settings link logout link to the header plus several helpers for the app name, tag and title and the company name
    
    changed the first menu header to a partial layout that lists the bank accounts and deleted the other menu hearders

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
     before_filter :require_no_user, :only => [:new, :create]
     before_filter :require_user, :only => [:show, :edit, :update]
     layout "loggedout_layout", :only => [:new,:create]
+    layout  "application", :only => [:show,:edit]
     def new
       @user = User.new
     end
