4220bb50efa8fb2e2a32300a960b3817eb5f7bb5
Manuel Silva <ishmael@blackbalto.com>
2009-06-05 17:06:07 +0100

    add the select look to the new bank account view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def app_title
-    "Phosney | Keeping your finances neatly in order.."  + account_selected.to_s
+    "Phosney | Keeping your finances neatly in order.." 
   end
   
   def app_name
--- app/helpers/bankaccounts_helper.rb
+++ app/helpers/bankaccounts_helper.rb
@@ -1,7 +1,11 @@
 module BankaccountsHelper
 	def accountnumber
 		if not @bankaccount.nil?
+			if @bankaccount.id.nil?
+			0
+			else
 			@bankaccount.id
+			end
 		end
 	end
 
