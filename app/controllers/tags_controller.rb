70422bc6d67c3dcc65c3e7d96b990c38a49ba2c5
Manuel Silva <ishmael@blackbalto.com>
2009-08-25 15:35:08 +0100

    fix a bug in acts as taggeable that allowed case sensitive searches and added the ability to delete tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -9,4 +9,14 @@ before_filter :require_user
     end
   end
 
+    def destroy
+    @tags = @current_user.tags.find(params[:id])
+    @tags.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(tags_url) }
+      format.xml  { head :ok }
+    end
+  end
+  
 end
