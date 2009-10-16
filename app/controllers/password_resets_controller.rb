5b0ea2e94378fa7612d69ff708147d8071ca1f74
Manuel Silva <ishmael@blackbalto.com>
2009-10-16 17:23:50 +0100

    recover password

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,6 +1,6 @@
 class PasswordResetsController < ApplicationController
 	before_filter :require_no_user
-	before_filter :load_user_using_perishable_token,only => [:edit, :update]  
+	before_filter :load_user_using_perishable_token,:only => [:edit, :update]  
 
 
 	def new
