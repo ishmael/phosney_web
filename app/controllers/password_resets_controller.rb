ae68cd94cf0e6524de955e465cc1e5d19d518a13
Manuel Silva <ishmael@blackbalto.com>
2009-10-16 17:22:08 +0100

    recover password

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,7 +1,6 @@
 class PasswordResetsController < ApplicationController
 	before_filter :require_no_user
-	before_filter :load_user_using_perishable_token,
-	<img src="http://www.binarylogic.com/wp-includes/images/smilies/icon_surprised.gif" alt=":o" class="wp-smiley"> nly => [:edit, :update]  
+	before_filter :load_user_using_perishable_token,only => [:edit, :update]  
 
 
 	def new
