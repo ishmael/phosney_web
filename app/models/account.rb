3d8656ccdfdcda724f760bfb38dcc5feb7693227
Manuel Silva <ishmael@blackbalto.com>
2009-10-27 15:11:59 +0000

    bug with account delete

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,5 @@
 class Account < ActiveRecord::Base
-  has_many :accounts_users
+  has_many :accounts_users, :dependent => :destroy
   has_many :users , :through => :accounts_users
   has_many :movements, :dependent => :destroy
   has_many :shared_account_invitations, :dependent => :destroy
