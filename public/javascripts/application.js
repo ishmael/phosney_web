db4d971ff01040779d573b07d5f1a271d702b9b2
Manuel Silva <ishmael@blackbalto.com>
2010-01-12 17:10:16 +0000

    change yui

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,85 +1,213 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-$(document).ready(function() {
-            $("#wideView").click(function() {
-                $("#content").toggleClass("wide");
-				$("#search").toggleClass("wide");
-				$("#primary").toggleClass("wide");
-                $(this).toggleClass("wide");
-            });
+(function () {
+	
+		var Event = YAHOO.util.Event,
+			Dom = YAHOO.util.Dom;	
 
-			$("#mov_type_debit")
-				.click(function () { 
-				var mov_type = document.getElementById("movement_mov_type");
-					mov_type.value = "-1";
-				});
+		var ButtonGroup = YAHOO.widget.ButtonGroup;
+		
+		
+		
+		Event.onContentReady("quickmovement_type_bgroup", function () {
+           var oButtonGroupQM = new ButtonGroup("quickmovement_type_bgroup");
+		   oButtonGroupQM.on("checkedButtonChange", onCheckedButtonChange);
+        });
+
+		 var onCheckedButtonChange = function (p_oEvent) {
+			var mov_type = document.getElementById("quickmovement_type");
 			
-			$("#mov_type_credit")
-				.click(function () { 
-				var mov_type = document.getElementById("movement_mov_type");
-					mov_type.value = "1";
-				});
-			var initial_mov_type = document.getElementById("movement_mov_type");
-			if (initial_mov_type.value == "-1")
-			{
-				$("#mov_type_debit").addClass("ui-state-active"); 
-			}
-			else if (initial_mov_type.value == "1")
-			{
-				$("#mov_type_credit").addClass("ui-state-active"); 
-			}
+            if(p_oEvent.newValue.get("id") == "qm_mov_debit") {
+				mov_type.value = "-1";      
+            }
+            if(p_oEvent.newValue.get("id") == "qm_mov_credit") {
+				mov_type.value = "1";
+            }
 			
-			
-			
+        };
+
+		Event.onContentReady("div-quickmovement-submit", function () {
+           var oPushButton2 = new YAHOO.widget.Button("qm-submit"); 
         });
 		
+		
+		Event.onContentReady("datefields", function () {
 
-		
-$(function(){
-	  $("#movement_movdate").datepicker({
-			changeMonth: true,
-			changeYear: true,
-			dateFormat: 'yy-mm-dd'
-		});
-		
-		$('select#movement_account_id').selectmenu({
-						style:'dropdown', 
-						menuWidth: 400,
-						width: 100
-					});
+			var oCalendarMenu;
+	
+			var onButtonClick = function () {
+	
+				// Create a Calendar instance and render it into the body 
+				// element of the Overlay.
+	
+				var oCalendar = new YAHOO.widget.Calendar("buttoncalendar", oCalendarMenu.body.id,{navigator:true});
+	
+				oCalendar.render();
+	
+	
+				// Subscribe to the Calendar instance's "select" event to 
+				// update the Button instance's label when the user
+				// selects a date.
+	
+				oCalendar.selectEvent.subscribe(function (p_sType, p_aArgs) {
+	
+					var aDate,
+						nMonth,
+						nDay,
+						nYear;
+	
+					if (p_aArgs) {
+						
+						aDate = p_aArgs[0][0];
+	
+						nMonth = aDate[1];
+						nDay = aDate[2];
+						nYear = aDate[0];
+	
+						oButton.set("label", ( nDay + "/" + nMonth + "/" + nYear));
+	
+	
+						// Sync the Calendar instance's selected date with the date form fields
+	
+						Dom.get("month").selectedIndex = (nMonth - 1);
+						Dom.get("day").selectedIndex = (nDay - 1);
+						Dom.get("year").value = nYear;
+	
+					}
 					
-		$('select#movement_category_id').selectmenu({
-			style:'dropdown', 
-			menuWidth: 400,
-			width: 100
-			});	
-			
-			$(".fg-button:not(.ui-state-disabled)")
-				.hover(
-					function(){ 
-						$(this).addClass("ui-state-hover"); 
-					},
-					function(){ 
-						$(this).removeClass("ui-state-hover"); 
-					}
-				)
-				.mousedown(function(){
-						$(this).parents('.fg-buttonset-single:first').find(".fg-button.ui-state-active").removeClass("ui-state-active");
-						if( $(this).is('.ui-state-active.fg-button-toggleable, .fg-buttonset-multi .ui-state-active') )
-						{
-							 $(this).removeClass("ui-state-active");
-							  
-						}
-						else { 
-							$(this).addClass("ui-state-active"); 
-							}	
-				})
-				.mouseup(function(){
-					if(! $(this).is('.fg-button-toggleable, .fg-buttonset-single .fg-button,  .fg-buttonset-multi .fg-button') ){
-						$(this).removeClass("ui-state-active");
-					}
+					oCalendarMenu.hide();
+				
 				});
-		});
+	
+	
+				// Pressing the Esc key will hide the Calendar Menu and send focus back to 
+				// its parent Button
+	
+				Event.on(oCalendarMenu.element, "keydown", function (p_oEvent) {
+				
+					if (Event.getCharCode(p_oEvent) === 27) {
+						oCalendarMenu.hide();
+						this.focus();
+					}
+				
+				}, null, this);
+				
+				
+				var focusDay = function () {
+
+					var oCalendarTBody = Dom.get("buttoncalendar").tBodies[0],
+						aElements = oCalendarTBody.getElementsByTagName("a"),
+						oAnchor;
+
+					
+					if (aElements.length > 0) {
+					
+						Dom.batch(aElements, function (element) {
+						
+							if (Dom.hasClass(element.parentNode, "today")) {
+								oAnchor = element;
+							}
+						
+						});
+						
+						
+						if (!oAnchor) {
+							oAnchor = aElements[0];
+						}
+
+
+						// Focus the anchor element using a timer since Calendar will try 
+						// to set focus to its next button by default
+						
+						YAHOO.lang.later(0, oAnchor, function () {
+							try {
+								oAnchor.focus();
+							}
+							catch(e) {}
+						});
+					
+					}
+					
+				};
+
+
+				// Set focus to either the current day, or first day of the month in 
+				// the Calendar	when it is made visible or the month changes
+	
+				oCalendarMenu.subscribe("show", focusDay);
+				oCalendar.renderEvent.subscribe(focusDay, oCalendar, true);
+	
+
+				// Give the Calendar an initial focus
+				
+				focusDay.call(oCalendar);
+	
+	
+				// Re-align the CalendarMenu to the Button to ensure that it is in the correct
+				// position when it is initial made visible
+				
+				oCalendarMenu.align();
+
+	
+				// Unsubscribe from the "click" event so that this code is 
+				// only executed once
+	
+				this.unsubscribe("click", onButtonClick);
 			
+			};
+	
+	
+			var oDateFields = Dom.get("datefields");
+				oMonthField = Dom.get("movement_movdate_2i"),
+				oDayField = Dom.get("movement_movdate_3i"),
+				oYearField = Dom.get("movement_movdate_1i");
+	
+	
+			// Hide the form fields used for the date so that they can be replaced by the 
+			// calendar button.
+	
+			oMonthField.style.display = "none";
+			oDayField.style.display = "none";
+			oYearField.style.display = "none";
+	
+	
+			// Create a Overlay instance to house the Calendar instance
+	
+			oCalendarMenu = new YAHOO.widget.Overlay("calendarmenu", { visible: false });
+	
+	
+			// Create a Button instance of type "menu"
+	
+			var oButton = new YAHOO.widget.Button({ 
+											type: "menu", 
+											id: "calendarpicker", 
+											label: oDayField.value+"/"+oMonthField.value+"/"+oYearField.value, 
+											menu: oCalendarMenu, 
+											container: "datefields" });
+	
+	
+			oButton.on("appendTo", function () {
+			
+				// Create an empty body element for the Overlay instance in order 
+				// to reserve space to render the Calendar instance into.
+		
+				oCalendarMenu.setBody("&#32;");
+		
+				oCalendarMenu.body.id = "calendarcontainer";
+			
+			});
+	
+	
+			// Add a listener for the "click" event.  This listener will be
+			// used to defer the creation the Calendar instance until the 
+			// first time the Button's Overlay instance is requested to be displayed
+			// by the user.
+	
+			oButton.on("click", onButtonClick);
+		
+		});
+
+	}());
+
 
 	
