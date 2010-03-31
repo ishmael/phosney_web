9166d9f5b62580d666777e994a02c9dd128a4d1f
ishmael <ishmael@blackbalto.com>
2010-03-31 18:51:27 +0100

    change way the database schema is saved

---
+++ db/development_structure.sql
@@ -0,0 +1,695 @@
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = off;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET escape_string_warning = off;
+
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: accounts_users; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE accounts_users (
+    id integer NOT NULL,
+    user_id integer NOT NULL,
+    account_id integer NOT NULL,
+    allow_delete numeric(1,0) DEFAULT 0,
+    allow_edit numeric(1,0) DEFAULT 0,
+    allow_insert numeric(1,0) DEFAULT 1,
+    created_at timestamp without time zone,
+    updated_at timestamp without time zone,
+    owner numeric(1,0) DEFAULT 0
+);
+
+
+--
+-- Name: account_holders_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE account_holders_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MAXVALUE
+    NO MINVALUE
+    CACHE 1;
+
+
+--
+-- Name: account_holders_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE account_holders_id_seq OWNED BY accounts_users.id;
+
+
+--
+-- Name: accounts; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE accounts (
+    id integer NOT NULL,
+    name character varying(255),
+    number character varying(255),
+    bank character varying(255),
+    created_at timestamp without time zone,
+    updated_at timestamp without time zone,
+    type character varying(255),
+    balance_in_cents integer DEFAULT 0,
+    currency character varying(255),
+    color character varying(255) DEFAULT '#000000'::character varying
+);
+
+
+--
+-- Name: bankaccounts_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE bankaccounts_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MAXVALUE
+    NO MINVALUE
+    CACHE 1;
+
+
+--
+-- Name: bankaccounts_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE bankaccounts_id_seq OWNED BY accounts.id;
+
+
+--
+-- Name: categories; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE categories (
+    id integer NOT NULL,
+    name character varying(255) NOT NULL,
+    user_id integer NOT NULL,
+    created_at timestamp without time zone,
+    updated_at timestamp without time zone,
+    parent_id integer DEFAULT 0,
+    mobile numeric(1,0) DEFAULT 0
+);
+
+
+--
+-- Name: categories_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE categories_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MAXVALUE
+    NO MINVALUE
+    CACHE 1;
+
+
+--
+-- Name: categories_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE categories_id_seq OWNED BY categories.id;
+
+
+--
+-- Name: movements; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE movements (
+    id integer NOT NULL,
+    account_id integer,
+    description character varying(255),
+    amount_in_cents integer DEFAULT 0,
+    movdate timestamp without time zone,
+    created_at timestamp without time zone,
+    created_on timestamp without time zone,
+    updated_at timestamp without time zone,
+    updated_on timestamp without time zone,
+    mov_type numeric(1,0) DEFAULT 1,
+    category_id integer,
+    lat double precision,
+    lng double precision,
+    accuracy double precision,
+    user_id integer NOT NULL,
+    private numeric(1,0) DEFAULT 0,
+    currency character varying(255)
+);
+
+
+--
+-- Name: movements_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE movements_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MAXVALUE
+    NO MINVALUE
+    CACHE 1;
+
+
+--
+-- Name: movements_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE movements_id_seq OWNED BY movements.id;
+
+
+--
+-- Name: schema_migrations; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE schema_migrations (
+    version character varying(255) NOT NULL
+);
+
+
+--
+-- Name: sessions; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE sessions (
+    id integer NOT NULL,
+    session_id character varying(255) NOT NULL,
+    data text,
+    created_at timestamp without time zone,
+    updated_at timestamp without time zone
+);
+
+
+--
+-- Name: sessions_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE sessions_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MAXVALUE
+    NO MINVALUE
+    CACHE 1;
+
+
+--
+-- Name: sessions_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE sessions_id_seq OWNED BY sessions.id;
+
+
+--
+-- Name: shared_account_invitations; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE shared_account_invitations (
+    id integer NOT NULL,
+    user_id integer NOT NULL,
+    recipient_email character varying(255),
+    token character varying(255),
+    account_id integer NOT NULL,
+    allow_delete numeric(1,0) DEFAULT 0,
+    allow_edit numeric(1,0) DEFAULT 0,
+    allow_insert numeric(1,0) DEFAULT 1,
+    sent_at timestamp without time zone,
+    created_at timestamp without time zone,
+    updated_at timestamp without time zone
+);
+
+
+--
+-- Name: shared_account_invitations_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE shared_account_invitations_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MAXVALUE
+    NO MINVALUE
+    CACHE 1;
+
+
+--
+-- Name: shared_account_invitations_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE shared_account_invitations_id_seq OWNED BY shared_account_invitations.id;
+
+
+--
+-- Name: taggings; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE taggings (
+    id integer NOT NULL,
+    tag_id integer,
+    taggable_id integer,
+    taggable_type character varying(255),
+    created_at timestamp without time zone
+);
+
+
+--
+-- Name: taggings_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE taggings_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MAXVALUE
+    NO MINVALUE
+    CACHE 1;
+
+
+--
+-- Name: taggings_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE taggings_id_seq OWNED BY taggings.id;
+
+
+--
+-- Name: tags; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE tags (
+    id integer NOT NULL,
+    name character varying(255),
+    user_id integer NOT NULL
+);
+
+
+--
+-- Name: tags_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE tags_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MAXVALUE
+    NO MINVALUE
+    CACHE 1;
+
+
+--
+-- Name: tags_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE tags_id_seq OWNED BY tags.id;
+
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE users (
+    id integer NOT NULL,
+    login character varying(255) NOT NULL,
+    email character varying(255) NOT NULL,
+    crypted_password character varying(255) NOT NULL,
+    password_salt character varying(255) NOT NULL,
+    persistence_token character varying(255) NOT NULL,
+    single_access_token character varying(255) NOT NULL,
+    perishable_token character varying(255) NOT NULL,
+    login_count integer DEFAULT 0 NOT NULL,
+    failed_login_count integer DEFAULT 0 NOT NULL,
+    last_request_at timestamp without time zone,
+    current_login_at timestamp without time zone,
+    last_login_at timestamp without time zone,
+    current_login_ip character varying(255),
+    last_login_ip character varying(255),
+    created_at timestamp without time zone,
+    updated_at timestamp without time zone,
+    locale character varying(255)
+);
+
+
+--
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE users_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MAXVALUE
+    NO MINVALUE
+    CACHE 1;
+
+
+--
+-- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE users_id_seq OWNED BY users.id;
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE accounts ALTER COLUMN id SET DEFAULT nextval('bankaccounts_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE accounts_users ALTER COLUMN id SET DEFAULT nextval('account_holders_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE categories ALTER COLUMN id SET DEFAULT nextval('categories_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE movements ALTER COLUMN id SET DEFAULT nextval('movements_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE sessions ALTER COLUMN id SET DEFAULT nextval('sessions_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE shared_account_invitations ALTER COLUMN id SET DEFAULT nextval('shared_account_invitations_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE taggings ALTER COLUMN id SET DEFAULT nextval('taggings_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE tags ALTER COLUMN id SET DEFAULT nextval('tags_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE users ALTER COLUMN id SET DEFAULT nextval('users_id_seq'::regclass);
+
+
+--
+-- Name: account_holders_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY accounts_users
+    ADD CONSTRAINT account_holders_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: bankaccounts_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY accounts
+    ADD CONSTRAINT bankaccounts_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: categories_name_ukey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY categories
+    ADD CONSTRAINT categories_name_ukey UNIQUE (user_id, name);
+
+
+--
+-- Name: categories_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY categories
+    ADD CONSTRAINT categories_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: movements_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY movements
+    ADD CONSTRAINT movements_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: sessions_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY sessions
+    ADD CONSTRAINT sessions_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: shared_account_invitations_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY shared_account_invitations
+    ADD CONSTRAINT shared_account_invitations_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: taggings_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY taggings
+    ADD CONSTRAINT taggings_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: tags_name_ukey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY tags
+    ADD CONSTRAINT tags_name_ukey UNIQUE (user_id, name);
+
+
+--
+-- Name: tags_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY tags
+    ADD CONSTRAINT tags_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: users_login_ukey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY users
+    ADD CONSTRAINT users_login_ukey UNIQUE (login);
+
+
+--
+-- Name: users_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: index_sessions_on_session_id; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX index_sessions_on_session_id ON sessions USING btree (session_id);
+
+
+--
+-- Name: index_sessions_on_updated_at; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX index_sessions_on_updated_at ON sessions USING btree (updated_at);
+
+
+--
+-- Name: index_taggings_on_tag_id; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX index_taggings_on_tag_id ON taggings USING btree (tag_id);
+
+
+--
+-- Name: index_taggings_on_taggable_id_and_taggable_type; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX index_taggings_on_taggable_id_and_taggable_type ON taggings USING btree (taggable_id, taggable_type);
+
+
+--
+-- Name: unique_schema_migrations; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE UNIQUE INDEX unique_schema_migrations ON schema_migrations USING btree (version);
+
+
+--
+-- Name: account_holders_account_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY accounts_users
+    ADD CONSTRAINT account_holders_account_id_fkey FOREIGN KEY (account_id) REFERENCES accounts(id);
+
+
+--
+-- Name: account_holders_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY accounts_users
+    ADD CONSTRAINT account_holders_user_id_fkey FOREIGN KEY (user_id) REFERENCES users(id);
+
+
+--
+-- Name: categories_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY categories
+    ADD CONSTRAINT categories_user_id_fkey FOREIGN KEY (user_id) REFERENCES users(id);
+
+
+--
+-- Name: movements_account_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY movements
+    ADD CONSTRAINT movements_account_id_fkey FOREIGN KEY (account_id) REFERENCES accounts(id);
+
+
+--
+-- Name: movements_category_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY movements
+    ADD CONSTRAINT movements_category_id_fkey FOREIGN KEY (category_id) REFERENCES categories(id);
+
+
+--
+-- Name: movements_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY movements
+    ADD CONSTRAINT movements_user_id_fkey FOREIGN KEY (user_id) REFERENCES users(id);
+
+
+--
+-- Name: shared_account_invitations_account_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY shared_account_invitations
+    ADD CONSTRAINT shared_account_invitations_account_id_fkey FOREIGN KEY (account_id) REFERENCES accounts(id);
+
+
+--
+-- Name: shared_account_invitations_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY shared_account_invitations
+    ADD CONSTRAINT shared_account_invitations_user_id_fkey FOREIGN KEY (user_id) REFERENCES users(id);
+
+
+--
+-- Name: taggins_tag_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY taggings
+    ADD CONSTRAINT taggins_tag_id_fkey FOREIGN KEY (tag_id) REFERENCES tags(id);
+
+
+--
+-- Name: tags_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY tags
+    ADD CONSTRAINT tags_user_id_fkey FOREIGN KEY (user_id) REFERENCES users(id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+INSERT INTO schema_migrations (version) VALUES ('20090424130844');
+
+INSERT INTO schema_migrations (version) VALUES ('20090424145448');
+
+INSERT INTO schema_migrations (version) VALUES ('20090427212203');
+
+INSERT INTO schema_migrations (version) VALUES ('20090428180043');
+
+INSERT INTO schema_migrations (version) VALUES ('20090606190058');
+
+INSERT INTO schema_migrations (version) VALUES ('20090612174928');
+
+INSERT INTO schema_migrations (version) VALUES ('20090613172253');
+
+INSERT INTO schema_migrations (version) VALUES ('20090613182008');
+
+INSERT INTO schema_migrations (version) VALUES ('20090617142545');
+
+INSERT INTO schema_migrations (version) VALUES ('20090729103833');
+
+INSERT INTO schema_migrations (version) VALUES ('20090729170935');
+
+INSERT INTO schema_migrations (version) VALUES ('20090731114654');
+
+INSERT INTO schema_migrations (version) VALUES ('20090731141025');
+
+INSERT INTO schema_migrations (version) VALUES ('20090814145502');
+
+INSERT INTO schema_migrations (version) VALUES ('20090817143834');
+
+INSERT INTO schema_migrations (version) VALUES ('20090826171144');
+
+INSERT INTO schema_migrations (version) VALUES ('20091017194410');
+
+INSERT INTO schema_migrations (version) VALUES ('20091017222742');
+
+INSERT INTO schema_migrations (version) VALUES ('20091017230728');
+
+INSERT INTO schema_migrations (version) VALUES ('20091019142647');
+
+INSERT INTO schema_migrations (version) VALUES ('20091022112815');
+
+INSERT INTO schema_migrations (version) VALUES ('20091022114424');
+
+INSERT INTO schema_migrations (version) VALUES ('20091022115434');
+
+INSERT INTO schema_migrations (version) VALUES ('20091022120839');
+
+INSERT INTO schema_migrations (version) VALUES ('20091022135409');
+
+INSERT INTO schema_migrations (version) VALUES ('20091022142400');
+
+INSERT INTO schema_migrations (version) VALUES ('20091023143448');
+
+INSERT INTO schema_migrations (version) VALUES ('20091026115450');
+
+INSERT INTO schema_migrations (version) VALUES ('20100317101206');
+
+INSERT INTO schema_migrations (version) VALUES ('20100318115922');
+
+INSERT INTO schema_migrations (version) VALUES ('20100318153038');
+
+INSERT INTO schema_migrations (version) VALUES ('20100322122353');
