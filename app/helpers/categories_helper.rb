6c46db420dbc654732d4a22af4ab5547e34db9cc
ishmael <ishmael@blackbalto.com>
2010-10-20 11:54:30 +0100

    first run of the new categories new

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,24 +1,66 @@
 module CategoriesHelper
-	def nodes(categories,parent_id)
+	def nodes(categories,parent_id,index)
 	  ret = ''
 	  tree= categories.select { |item| item.parent_id == parent_id } 
-	  tree.sort_by(&:name).each do |categoria|
-      if  (categoria.user_id == @current_user.id)
-    	  ret += '<li class="moveable droppable "><div class="categoryname drag-button"><div class="categorytext">'+  link_to( categoria.name,category_path(categoria.id))+ '</div>'
-    	  ret +=  link_to image_tag("/images/led-ico/delete.png",:alt =>I18n.t('layout.application.delete')),  category_path(categoria.id) , {:confirm => I18n.t('layout.application.deletemessage'), :method => :delete ,:class => 'ico' , :title => I18n.t('layout.application.delete')} 
-    		ret +=  link_to image_tag("/images/led-ico/pencil.png",:alt => I18n.t('layout.application.edit')), edit_category_path(categoria.id),{:class  =>'ico', :title => I18n.t('layout.application.edit')} 
-    		ret +=  link_to image_tag("/images/led-ico/arrow_divide.png",:alt => I18n.t('layout.application.share')), new_polymorphic_path([categoria,:categories_user]),{:class  =>'ico', :title => I18n.t('layout.application.share')} 
-  	  else
-    	  ret += '<li><div class="categoryname drag-button"><div class="categorytext">'+  link_to( categoria.name,category_path(categoria.id))+ '</div>'
-      end 	  
-  	  ret +=  '</div>'
-  	  if  (categoria.user_id == @current_user.id)
-    		ret += "<ul id=\"" +categoria.id.to_s+ "\" class=\"catnestedlist droppable\">"
-  		  ret+= nodes(categories,categoria.id)
-    		ret +='</ul>'
-  	  end
-    		ret += '</li>' 
-    end 
+	  unicos = tree.uniq
+	  unicos.sort_by(&:name).each_with_index do |categoria,indice|
+	      totais_categoria = categories.select { |cate| cate.id == categoria.id } 
+	      totais_hash = Hash.new
+	      
+	      totais_categoria.each do |total|
+	        if totais_hash.has_key?(total.currency)
+	          total_linha = totais_hash.fetch(total.currency)
+	        else
+	          total_linha = Array.new(6,0)
+	        end
+	          total_linha[0]+= total.month5.to_i
+	          total_linha[1]+=total.month4.to_i
+	          total_linha[2]+=total.month3.to_i
+	          total_linha[3]+=total.month2.to_i
+	          total_linha[4]+=total.month1.to_i
+	          total_linha[5]+=total.month0.to_i
+	          totais_hash[total.currency] = total_linha          	          	          	          	        	      
+
+	      end
+	      cssclass = ''
+	       if indice== 0 and index==1 
+	        cssclass= 'first'
+	       else 
+	         cssclass=  cycle('even', 'odd',:name=> "categories") 
+	      end
+	      ret += '<tr class="'+cssclass+'">'
+        ret += '<td  style="padding-left:'+(10*index).to_s+'px">'+ categoria.name + '</td>'
+            soma_linha = Array.new(6,'')
+            totais_hash.each_pair {|key, value|  
+
+                  total_individual = value
+                  
+                  total_individual.each_with_index do |tot_indiv,i|
+                    if key !=nil and tot_indiv != 0
+    					        soma_linha[i]+= Money.new(tot_indiv,key).format + '<br>'
+    					      else
+    					        soma_linha[i]+= '0' + '<br>'
+                    end  
+    				      end
+
+    				}										
+    				soma_linha.each do |soma|
+                    ret+='<td class="categoryamount">'  + soma  				
+	          				ret+='</td'
+	          end
+					    ret +='<td>  '
+					    if  (categoria.user_id == @current_user.id)		
+    						ret +=	'<ul class="actions">'
+    						ret +=		'<li>' + link_to( image_tag("/images/led-ico/delete.png",:alt =>I18n.t('layout.application.delete')),  new_category_path , {:confirm => I18n.t('layout.application.deletemessage'), :method => :delete ,:class => 'ico' , :title => I18n.t('layout.application.delete')}) +'</li>'
+						    ret +=	'<li>'+ 	link_to(image_tag("/images/led-ico/pencil.png",:alt => I18n.t('layout.application.edit')), new_category_path,{:class  =>'ico', :title => I18n.t('layout.application.edit')}) + '</li>'
+						    ret +=	'<li>' +  link_to(image_tag("/images/led-ico/arrow_divide.png",:alt => I18n.t('layout.application.share')), new_category_path,{:class  =>'ico', :title => I18n.t('layout.application.share')}) +'</li>'
+             		ret +=	'</ul>'
+              end
+						ret +='</td>'
+					ret +='</tr>'
+          ret +=nodes(categories,categoria.id,index+1)
+    end
+    
     return ret
 	end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -7,7 +7,25 @@ class Category < ActiveRecord::Base
   validates_presence_of :name
   validates_uniqueness_of :name, :scope => :user_id , :message =>  I18n.t('layout.categories.duplicate')
   default_scope :order => :name
-  named_scope :sharedcats,  :select =>  "categories.id, case when categories.user_id = categories_users.user_id then categories.name else categories.name || '(' || (select users.login from users where users.id = categories.user_id) || ')' end as name,categories.user_id,categories.created_at,categories.updated_at,categories.parent_id,categories.mobile",:joins => "INNER JOIN categories_users ON categories.id = categories_users.category_id"
+  named_scope :sharedcats,  :select =>  "categories.id,
+  	categories.parent_id,
+  	categories.user_id ,
+  	cate_moves.account_id,
+  	cate_moves.currency,
+  	coalesce((select sum(amount_in_cents*mov_type) from movements where movements.category_id = categories.id and movements.account_id = cate_moves.account_id and  date_trunc('month',movdate) = date_trunc('month',now() + -5 * interval '1 month') ),0) as month5,
+  	coalesce((select sum(amount_in_cents*mov_type) from movements where movements.category_id = categories.id and movements.account_id = cate_moves.account_id and  date_trunc('month',movdate) = date_trunc('month',now() + -4 * interval '1 month') ),0) as month4,
+  	coalesce((select sum(amount_in_cents*mov_type) from movements where movements.category_id = categories.id and movements.account_id = cate_moves.account_id and  date_trunc('month',movdate) = date_trunc('month',now() + -3 * interval '1 month') ),0) as month3,
+  	coalesce((select sum(amount_in_cents*mov_type) from movements where movements.category_id = categories.id and movements.account_id = cate_moves.account_id and  date_trunc('month',movdate) = date_trunc('month',now() + -2 * interval '1 month') ),0) as month2,
+  	coalesce((select sum(amount_in_cents*mov_type) from movements where movements.category_id = categories.id and movements.account_id = cate_moves.account_id and  date_trunc('month',movdate) = date_trunc('month',now() + -1 * interval '1 month') ),0) as month1,
+  	coalesce((select sum(amount_in_cents*mov_type) from movements where movements.category_id = categories.id and movements.account_id = cate_moves.account_id and  date_trunc('month',movdate) = date_trunc('month',now() + 0 * interval '1 month') ),0) as month0,
+  	case when categories.user_id = categories_users.user_id then categories.name else categories.name || '(' || (select users.login from users where users.id = categories.user_id) || ')' end as name,
+  	cate_moves.name as account_name,
+  	cate_moves.color",
+  	:joins => "INNER JOIN categories_users ON categories.id = categories_users.category_id 
+  	  left outer join (select account_id,category_id,date_trunc('month',movdate) as movdate  ,accounts.currency,accounts.name,accounts.color from movements ,accounts
+      	where category_id is not null and date_trunc('month',movdate) > date_trunc('month',CURRENT_TIMESTAMP - interval '5 month') and accounts.id = movements.account_id
+      	group by account_id,category_id,date_trunc('month',movdate),accounts.currency,accounts.name,accounts.color ) cate_moves on categories.id = cate_moves.category_id",
+  	:order => "categories.name"
 
   #acts_as_tree 
 
